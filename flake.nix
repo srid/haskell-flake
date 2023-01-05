b4b9ad9e00025b2a7b3d3996419eca0ab4caf243
Sridhar Ratnakumar <srid@srid.ca>
2023-01-05 18:28:59 +0530

    Use nixpkgs-lib input; tempalte requires it

--- flake.nix
+++ flake.nix
@@ -1,7 +1,14 @@
 {
   description = "A `flake-parts` module for Haskell development";
-  outputs = { self, ... }: {
+  inputs = {
+    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
+  };
+  outputs = { self, nixpkgs-lib, ... }: {
     flakeModule = ./flake-module.nix;
+    templates.default.path = (nixpkgs-lib.lib.cleanSourceWith {
+      src = ./example;
+      filter = path: type: baseNameOf path == "flake.nix";
+    }).outPath;
     herculesCI.ciSystems = [ "x86_64-linux" "aarch64-darwin" ];
   };
 }
