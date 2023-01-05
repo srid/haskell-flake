dcac96dcd15b8a424e9b86d4543ce1d46b4eef46
Sridhar Ratnakumar <srid@srid.ca>
2023-01-05 06:20:12 +0530

    Add HCI systems

--- flake.nix
+++ flake.nix
@@ -2,5 +2,6 @@
   description = "A `flake-parts` module for Haskell development";
   outputs = { self, ... }: {
     flakeModule = ./flake-module.nix;
+    herculesCI.ciSystems = [ "x86_64-linux" "aarch64-darwin" ];
   };
 }
