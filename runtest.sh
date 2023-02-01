29950e783f040dfe19c6384b061f3759d8918a05
Sridhar Ratnakumar <srid@srid.ca>
2023-01-31 19:42:50 -0500

    cleanup

--- runtest.sh
+++ runtest.sh
@@ -2,11 +2,9 @@ FLAKE=$(pwd)
 cd ./test
 # First, build the flake.
 nix build --override-input haskell-flake path:${FLAKE}
-# Run the test script in a nix develop shell.
+# Run the devshell test script in a nix develop shell.
 nix develop --override-input haskell-flake path:${FLAKE} -c ./test.sh
 # Test non-devshell features:
 # Checks
 nix --option sandbox false \
     build --override-input haskell-flake path:${FLAKE} -L .#check
-
-# TODO: test multi package.
