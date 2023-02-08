421b7f9bbbb7fe3fe5af2a1b0a2166342daa88fb
Sridhar Ratnakumar <srid@srid.ca>
2023-02-08 17:04:33 -0500

    Make runtest.sh work on macOS
    - `-e` is not recognized by macOS's zsh's echo command
    - Use current system when building the check

--- runtest.sh
+++ runtest.sh
@@ -1,23 +1,35 @@
 set -e
 
+if [ "$(uname)" == "Darwin" ]; then
+    SYSTEM=aarch64-darwin
+    function logHeader {
+        echo "\n||| $@"
+    }
+else
+    SYSTEM=x86_64-linux
+    function logHeader {
+        echo -e "\n||| $@"
+    }
+fi
+
 FLAKE=$(pwd)
 cd ./test
 
 # First, build the flake.
-echo -e "\n||| Testing nix build"
+logHeader "Testing nix build"
 nix build --override-input haskell-flake path:${FLAKE}
 # Run the devshell test script in a nix develop shell.
-echo -e "\n||| Testing nix devshell"
+logHeader "Testing nix devshell"
 nix develop --override-input haskell-flake path:${FLAKE} -c ./test.sh
 # Test non-devshell features:
 # Checks
-echo -e "\n||| Testing nix flake checks"
+logHeader "Testing nix flake checks"
 nix --option sandbox false \
     build --override-input haskell-flake path:${FLAKE} -L .#check
 
-echo -e "\n||| Testing docs"
+logHeader "Testing docs"
 nix build --override-input haskell-flake path:${FLAKE} \
     --option log-lines 1000 --show-trace \
-    github:hercules-ci/flake.parts-website#checks.x86_64-linux.linkcheck
+    github:hercules-ci/flake.parts-website#checks.${SYSTEM}.linkcheck
 
-echo -e "\n||| All tests passed!"
+logHeader "All tests passed!"
