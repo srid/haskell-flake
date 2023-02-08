c1a9b684b8dc6ec27c76e186810fc241b0191782
sbh69840 <sbh69840@gmail.com>
2023-02-08 19:20:27 +0530

    flake.nix: Created flake-only template

--- flake.nix
+++ flake.nix
@@ -6,5 +6,9 @@
       description = "Example project using haskell-flake";
       path = ./example;
     };
+    templates.flake-only = {
+      description = "Example flake using haskell-flake";
+      path = builtins.path {path=./example; filter = path: _: baseNameOf path == "flake.nix";};
+    };
   };
 }
