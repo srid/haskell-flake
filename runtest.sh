0f3e97be6a6e8b07c9e9ad1afe00f035db36d1e7
Robert Hensing <robert@roberthensing.nl>
2023-02-08 16:32:39 +0100

    runtest.sh: Actually print newlines

--- runtest.sh
+++ runtest.sh
@@ -4,20 +4,20 @@ FLAKE=$(pwd)
 cd ./test
 
 # First, build the flake.
-echo "\n||| Testing nix build"
+echo -e "\n||| Testing nix build"
 nix build --override-input haskell-flake path:${FLAKE}
 # Run the devshell test script in a nix develop shell.
-echo "\n||| Testing nix devshell"
+echo -e "\n||| Testing nix devshell"
 nix develop --override-input haskell-flake path:${FLAKE} -c ./test.sh
 # Test non-devshell features:
 # Checks
-echo "\n||| Testing nix flake checks"
+echo -e "\n||| Testing nix flake checks"
 nix --option sandbox false \
     build --override-input haskell-flake path:${FLAKE} -L .#check
 
-echo "\n||| Testing docs"
+echo -e "\n||| Testing docs"
 nix build --override-input haskell-flake path:${FLAKE} \
     --option log-lines 1000 --show-trace \
     github:hercules-ci/flake.parts-website#checks.x86_64-linux.linkcheck
 
-echo "\n||| All tests passed!"
+echo -e "\n||| All tests passed!"
