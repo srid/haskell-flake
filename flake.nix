f782bb202499239a4435a1407d7ff409d34866a0
Sridhar Ratnakumar <srid@srid.ca>
2023-02-09 20:55:35 -0500

    new template spec
    https://github.com/srid/haskell-flake/pull/69#discussion_r1101566043

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
   outputs = { self, ... }: {
     flakeModule = ./flake-module.nix;
     templates.default = {
-      description = "Example project using haskell-flake";
-      path = ./example;
-    };
-    templates.flake-only = {
-      description = "Example flake using haskell-flake";
+      description = "A simple flake.nix using haskell-flake";
       path = builtins.path { path = ./example; filter = path: _: baseNameOf path == "flake.nix"; };
+    };
+    templates.example = {
+      description = "Example Haskell project using haskell-flake";
+      path = ./example;
     };
   };
 }
