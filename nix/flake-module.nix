1e2d5b85a2ffa4d1734b903f0bc340fcad08559d
Sridhar Ratnakumar <srid@srid.ca>
2023-03-06 13:44:39 -0500

    Make it work

--- nix/flake-module.nix
+++ nix/flake-module.nix
@@ -3,7 +3,8 @@
 
 let
   inherit (flake-parts-lib)
-    mkPerSystemOption;
+    mkPerSystemOption
+    mkSubmoduleOptions;
   inherit (lib)
     mkOption
     types;
@@ -226,43 +227,7 @@ in
               type = types.attrsOf projectSubmodule;
             };
 
-            haskellFlakeProjectModules = mkOption {
-              type = types.lazyAttrsOf types.deferredModule;
-              description = ''
-                An attrset of `haskellProjects.<name>` modules that can be imported in
-                other flakes.
-              '';
-              defaultText = ''
-                `flake.haskellFlakeProjectModules` modules for this project,
-                for reuse in another flake, when using this project as a
-                Haskell dependency.
 
-                Typically the consumer of this flake will want to use one of the
-                following modules:
-
-                  - output: provides both local package and dependency overrides.
-                  - local: provides only local package overrides (ignores dependency
-                    overrides in this flake)
-              '';
-              default = rec {
-                # The 'output' module provides both local package and dependency
-                # overrides.
-                output = {
-                  imports = [ input local ];
-                };
-                # The 'local' module provides only local package overrides.
-                local = { pkgs, lib, ... }: withSystem pkgs.system ({ config, ... }: {
-                  source-overrides =
-                    lib.mapAttrs (_: v: v.root)
-                      config.haskellProjects.default.packages;
-                });
-                # The 'input' module contains only dependency overrides.
-                input = { pkgs, ... }: withSystem pkgs.system ({ config, ... }: {
-                  inherit (config.haskellProjects.default)
-                    source-overrides overrides;
-                });
-              };
-            };
           };
 
           config =
@@ -301,5 +266,45 @@ in
                   config.haskellProjects;
             };
         });
+
+    flake = mkSubmoduleOptions {
+      haskellFlakeProjectModules = mkOption {
+        type = types.lazyAttrsOf types.deferredModule;
+        description = ''
+          An attrset of `haskellProjects.<name>` modules that can be imported in
+          other flakes.
+        '';
+        defaultText = ''
+          `flake.haskellFlakeProjectModules` modules for this project,
+          for reuse in another flake, when using this project as a
+          Haskell dependency.
+
+          Typically the consumer of this flake will want to use one of the
+          following modules:
+
+            - output: provides both local package and dependency overrides.
+            - local: provides only local package overrides (ignores dependency
+              overrides in this flake)
+        '';
+        default = rec {
+          # The 'output' module provides both local package and dependency
+          # overrides.
+          output = {
+            imports = [ input local ];
+          };
+          # The 'local' module provides only local package overrides.
+          local = { pkgs, lib, ... }: withSystem pkgs.system ({ config, ... }: {
+            source-overrides =
+              lib.mapAttrs (_: v: v.root)
+                config.haskellProjects.default.packages;
+          });
+          # The 'input' module contains only dependency overrides.
+          input = { pkgs, ... }: withSystem pkgs.system ({ config, ... }: {
+            inherit (config.haskellProjects.default)
+              source-overrides overrides;
+          });
+        };
+      };
+    };
   };
 }
