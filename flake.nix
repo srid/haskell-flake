e6c73f1d573070eb69dd9bc9232c384108eb6e37
Sridhar Ratnakumar <srid@srid.ca>
2023-03-09 17:35:40 -0500

    redundant

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "A `flake-parts` module for Haskell development";
   outputs = { self, ... }: {
     flakeModule = ./nix/flake-module.nix;
-    tests = ./nix/parser_tests.nix;
     templates.default = {
       description = "A simple flake.nix using haskell-flake";
       path = builtins.path { path = ./example; filter = path: _: baseNameOf path == "flake.nix"; };
