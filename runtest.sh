dcfcda04cbaf885ff9b87eafc612ffea73872778
Sridhar Ratnakumar <srid@srid.ca>
2023-03-13 20:39:27 -0400

    debug

--- runtest.sh
+++ runtest.sh
@@ -16,7 +16,7 @@ else
     }
 fi
 FLAKE=$(pwd)
-OVERRIDE_HASKELL_FLAKE="--override-input haskell-flake path:${FLAKE}"
+OVERRIDE_HASKELL_FLAKE="--quiet --override-input haskell-flake path:${FLAKE}"
 
 # Export the common library.
 export -f logHeader
@@ -27,6 +27,7 @@ export FLAKE SYSTEM OVERRIDE_HASKELL_FLAKE
 # - https://github.com/NixOS/nix/issues/7263
 # - https://github.com/NixOS/nix/issues/7026
 export PATH=$(nix eval --raw github:nixos/nix/2.14.1#default.outPath)/bin:$PATH
+echo $PATH
 nix --version
 
 # Before anything, run the main haskell-flake tests
