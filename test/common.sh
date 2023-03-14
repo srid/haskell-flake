ed86b698d9f9d0b66a075e1beb2d14e3b7615a9e
Sridhar Ratnakumar <srid@srid.ca>
2023-03-13 20:52:31 -0400

    Must build the nix to get outPath

--- test/common.sh
+++ test/common.sh
@@ -21,6 +21,7 @@ OVERRIDE_HASKELL_FLAKE="--quiet --override-input haskell-flake path:${HASKELL_FL
 # We use newer Nix for:
 # - https://github.com/NixOS/nix/issues/7263
 # - https://github.com/NixOS/nix/issues/7026
+nix build --no-link github:nixos/nix/2.14.1
 export PATH=$(nix eval --raw github:nixos/nix/2.14.1#default.outPath)/bin:$PATH
 echo $PATH
 nix --version
