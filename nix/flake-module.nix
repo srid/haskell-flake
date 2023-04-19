3ee7c78ad830dbd50ac537ea2d8f09aa0a83000d
Sridhar Ratnakumar <srid@srid.ca>
2023-04-19 14:17:31 -0400

    Decouple appType

--- nix/flake-module.nix
+++ nix/flake-module.nix
@@ -16,6 +16,7 @@ in
     perSystem = mkPerSystemOption
       ({ config, self', inputs', pkgs, system, ... }:
         let
+          appType = pkgs.callPackage ./app-type.nix { };
           hlsCheckSubmodule = types.submodule {
             options = {
               enable = mkOption {
@@ -159,30 +160,7 @@ in
 
             };
           };
-          derivationType = lib.types.package // {
-            check = lib.isDerivation;
-          };
 
-          getExe = x:
-            "${lib.getBin x}/bin/${x.meta.mainProgram or (throw ''Package ${x.name or ""} does not have meta.mainProgram set, so I don't know how to find the main executable. You can set meta.mainProgram, or pass the full path to executable, e.g. program = "''${pkg}/bin/foo"'')}";
-          programType = lib.types.coercedTo derivationType getExe lib.types.str;
-          appType = lib.types.submodule {
-            options = {
-              type = mkOption {
-                type = lib.types.enum [ "app" ];
-                default = "app";
-                description = ''
-                  A type tag for `apps` consumers.
-                '';
-              };
-              program = mkOption {
-                type = programType;
-                description = ''
-                  A path to an executable or a derivation with `meta.mainProgram`.
-                '';
-              };
-            };
-          };
           packageInfoSubmodule = types.submoduleWith {
             modules = [
               ({ config, ... }: {
