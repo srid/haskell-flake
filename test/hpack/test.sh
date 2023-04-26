32e914a83f082eb9267d16d6c014c96261970ec2
Sridhar Ratnakumar <srid@srid.ca>
2023-04-26 14:43:47 -0400

    Remove automatic hpack generation

--- test/hpack/test.sh
+++
@@ -1,9 +0,0 @@
-source ../common.sh
-set -euxo pipefail
-
-# First, build the flake
-logHeader "Testing nix build"
-nix build ${OVERRIDE_ALL}
-# Run the devshell test script in a nix develop shell.
-logHeader "Testing nix devshell"
-nix develop ${OVERRIDE_ALL} -c echo
