e32392d1d9984f66cd6384074e8fb23a88e3e5b6
Sridhar Ratnakumar <srid@srid.ca>
2023-04-26 17:40:32 -0400

    Add a 'debug' option to log drv hashes at all stages

--- nix/build-haskell-package.nix
+++ nix/build-haskell-package.nix
@@ -1,9 +1,11 @@
 # Like callCabal2nix, but does more:
 # - Source filtering (to prevent parent content changes causing rebuilds)
 # - Always build from cabal's sdist for release-worthiness
-{ pkgs, lib, self, ... }:
+{ pkgs, lib, self, debug, ... }:
 
 let
+  log = import ./logging.nix { inherit lib debug; };
+
   fromSdist = self.buildFromCabalSdist or (builtins.trace "Your version of Nixpkgs does not support hs.buildFromCabalSdist yet." (pkg: pkg));
 
   mkNewStorePath = name: src:
@@ -23,12 +25,13 @@ lib.pipe pkgCfg.root
   [
     # Avoid rebuilding because of changes in parent directories
     (mkNewStorePath "source-${name}")
-
-    (x: builtins.trace x.outPath x)
+    (log.traceDebug "${name}.mkNewStorePath" (x: x.outPath))
 
     (root: self.callCabal2nix name root { })
+    (log.traceDebug "${name}.cabal2nixDeriver" (x: x.cabal2nixDeriver.outPath))
 
     # Make sure all files we use are included in the sdist, as a check
     # for release-worthiness.
     fromSdist
+    (log.traceDebug "${name}.fromSdist" (x: x.outPath))
   ]
