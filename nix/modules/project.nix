600581759870e813ff49e0b0d20fcada7cee88a7
Sridhar Ratnakumar <srid@srid.ca>
2023-04-28 12:49:35 -0400

    decouple devshell

--- nix/modules/project.nix
+++ nix/modules/project.nix
@@ -5,7 +5,6 @@ let
     mkOption
     types;
   inherit (types)
-    functionTo
     raw;
 
   appType = import ../types/app-type.nix { inherit pkgs lib; };
@@ -22,57 +21,7 @@ let
       };
     };
   };
-  devShellSubmodule = types.submodule {
-    options = {
-      enable = mkOption {
-        type = types.bool;
-        description = ''
-          Whether to enable a development shell for the project.
-        '';
-        default = true;
-      };
-      tools = mkOption {
-        type = functionTo (types.attrsOf (types.nullOr types.package));
-        description = ''
-          Build tools for developing the Haskell project.
-        '';
-        default = hp: { };
-        defaultText = ''
-          Build tools useful for Haskell development are included by default.
-        '';
-      };
-      extraLibraries = mkOption {
-        type = functionTo (types.attrsOf (types.nullOr types.package));
-        description = ''
-          Extra Haskell libraries available in the shell's environment.
-          These can be used in the shell's `runghc` and `ghci` for instance.
 
-          The argument is the Haskell package set.
-
-          The return type is an attribute set for overridability and syntax, as only the values are used.
-        '';
-        default = hp: { };
-        defaultText = lib.literalExpression "hp: { }";
-        example = lib.literalExpression "hp: { inherit (hp) releaser; }";
-      };
-
-      mkShellArgs = mkOption {
-        type = types.attrsOf types.raw;
-        description = ''
-          Extra arguments to pass to `pkgs.mkShell`.
-        '';
-        default = { };
-        example = ''
-          {
-            shellHook = \'\'
-              # Re-generate .cabal files so HLS will work (per hie.yaml)
-              ''${pkgs.findutils}/bin/find -name package.yaml -exec hpack {} \;
-            \'\';
-          };
-        '';
-      };
-    };
-  };
   outputsSubmodule = types.submodule {
     options = {
       finalOverlay = mkOption {
@@ -106,13 +55,6 @@ let
           Flake apps for each Cabal executable in the project.
         '';
       };
-      devShell = mkOption {
-        type = types.package;
-        readOnly = true;
-        description = ''
-          The development shell derivation generated for this project.
-        '';
-      };
     };
   };
 
@@ -141,7 +83,7 @@ let
 in
 {
   imports = [
-    ./project/hls-check.nix
+    ./project/devshell.nix
   ];
   options = {
     projectRoot = mkOption {
@@ -255,13 +197,6 @@ in
         ];
       defaultText = lib.literalMD "autodiscovered by reading `self` files.";
     };
-    devShell = mkOption {
-      type = devShellSubmodule;
-      description = ''
-        Development shell configuration
-      '';
-      default = { };
-    };
     outputs = mkOption {
       type = outputsSubmodule;
       description = ''
@@ -310,31 +245,6 @@ in
         config.overrides
       ];
 
-      defaultBuildTools = hp: with hp; {
-        inherit
-          cabal-install
-          haskell-language-server
-          ghcid
-          hlint;
-      };
-      nativeBuildInputs = lib.attrValues (defaultBuildTools finalPackages // config.devShell.tools finalPackages);
-      mkShellArgs = config.devShell.mkShellArgs // {
-        nativeBuildInputs = (config.devShell.mkShellArgs.nativeBuildInputs or [ ]) ++ nativeBuildInputs;
-      };
-      devShell = finalPackages.shellFor (mkShellArgs // {
-        packages = p:
-          map
-            (name: p."${name}")
-            (lib.attrNames config.packages);
-        withHoogle = true;
-        extraDependencies = p:
-          let o = mkShellArgs.extraDependencies or (_: { }) p;
-          in o // {
-            libraryHaskellDepends = o.libraryHaskellDepends or [ ]
-              ++ builtins.attrValues (config.devShell.extraLibraries p);
-          };
-      });
-
       buildPackageInfo = name: value: {
         package = finalPackages.${name};
         exes =
@@ -363,7 +273,7 @@ in
     in
     {
       outputs = {
-        inherit devShell finalOverlay;
+        inherit finalOverlay;
 
         finalPackages = config.basePackages.extend finalOverlay;
 
@@ -372,8 +282,6 @@ in
         apps =
           lib.mkMerge
             (lib.mapAttrsToList (_: packageInfo: packageInfo.exes) packages);
-
       };
-
     };
 }
