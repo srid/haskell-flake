8667886a8e7cac7f1099bcb1580f79246a688bfe
Sridhar Ratnakumar <srid@srid.ca>
2023-05-16 13:59:33 -0400

    refacto: package.nix

---
+++ nix/modules/project/package.nix
@@ -0,0 +1,82 @@
+{ description, default ? null, lib, pkgs, ... }:
+let
+  inherit (lib)
+    mkOption
+    types;
+
+  packageOptions = { config, ... }: {
+    options = {
+      root = mkOption {
+        type = types.nullOr types.path;
+        description = ''
+          Path containing the Haskell package's `.cabal` file.
+        '';
+        default = null;
+      };
+
+      # cabal2nix stuff goes here.
+
+      check = mkOption {
+        type = types.nullOr types.bool;
+        description = ''
+          Whether to run cabal tests as part of the nix build
+        '';
+        default = null;
+      };
+
+      haddock = mkOption {
+        type = types.nullOr types.bool;
+        description = ''
+          Whether to generate haddock documentation as part of the nix build
+        '';
+        default = null;
+      };
+
+      extraBuildDepends = mkOption {
+        type = types.nullOr (types.listOf types.package);
+        description = ''
+          Extra build dependencies for the package.
+        '';
+        default = null;
+      };
+
+      apply = mkOption {
+        type = types.functionTo types.package;
+        internal = true;
+        readOnly = true;
+        default = with pkgs.haskell.lib.compose;
+          lib.flip lib.pipe (
+            lib.optional (config.check != null)
+              (if config.check then doCheck else dontCheck)
+            ++
+            lib.optional (config.haddock != null)
+              (if config.haddock then doHaddock else dontHaddock)
+            ++
+            lib.optional (config.extraBuildDepends != null && config.extraBuildDepends != [ ])
+              (addBuildDepends config.extraBuildDepends)
+          );
+      };
+    };
+  };
+
+  packageSubmodule = types.deferredModuleWith {
+    staticModules = [
+      packageOptions
+    ];
+  };
+
+  packagesType = types.lazyAttrsOf packageSubmodule;
+in
+lib.mkOption {
+  inherit description default;
+  type = packagesType;
+  apply = packages:
+    lib.mapAttrs
+      (k: v:
+        (lib.evalModules {
+          modules = [ v ];
+          specialArgs = { inherit pkgs; };
+        }).config
+      )
+      packages;
+}
--- nix/modules/project/packages.nix
+++ nix/modules/project/packages.nix
@@ -15,94 +15,23 @@ let
       Please specify the `packages` option manually or change your project configuration (cabal.project).
     '';
   };
-
-  packageOptions = { config, ... }: {
-    options = {
-      root = mkOption {
-        type = types.nullOr types.path;
-        description = ''
-          Path containing the Haskell package's `.cabal` file.
-        '';
-        default = null;
-      };
-
-      # cabal2nix stuff goes here.
-
-      check = mkOption {
-        type = types.nullOr types.bool;
-        description = ''
-          Whether to run cabal tests as part of the nix build
-        '';
-        default = null;
-      };
-
-      haddock = mkOption {
-        type = types.nullOr types.bool;
-        description = ''
-          Whether to generate haddock documentation as part of the nix build
-        '';
-        default = null;
-      };
-
-      extraBuildDepends = mkOption {
-        type = types.nullOr (types.listOf types.package);
-        description = ''
-          Extra build dependencies for the package.
-        '';
-        default = null;
-      };
-
-      apply = mkOption {
-        type = types.functionTo types.package;
-        internal = true;
-        readOnly = true;
-        default = with pkgs.haskell.lib.compose;
-          lib.flip lib.pipe (
-            lib.optional (config.check != null)
-              (if config.check then doCheck else dontCheck)
-            ++
-            lib.optional (config.haddock != null)
-              (if config.haddock then doHaddock else dontHaddock)
-            ++
-            lib.optional (config.extraBuildDepends != null && config.extraBuildDepends != [ ])
-              (addBuildDepends config.extraBuildDepends)
-          );
-      };
-    };
-  };
-
-  packageSubmodule = types.deferredModuleWith {
-    staticModules = [
-      packageOptions
-    ];
-  };
-
 in
 {
   options = {
-    packages = mkOption {
-      type = types.lazyAttrsOf packageSubmodule;
-      apply = packages:
-        lib.mapAttrs
-          (k: v:
-            (lib.evalModules {
-              modules = [ v ];
-              specialArgs = { inherit pkgs; };
-            }).config
-          )
-          packages;
-      description = ''
-        Set of local packages in the project repository.
+    packages =
+      import ./package.nix {
+        inherit lib pkgs;
+        description = ''
+          Set of local packages in the project repository.
 
-        If you have a `cabal.project` file (under `projectRoot`), those packages
-        are automatically discovered. Otherwise, the top-level .cabal file is
-        used to discover the only local package.
+          If you have a `cabal.project` file (under `projectRoot`), those packages
+          are automatically discovered. Otherwise, the top-level .cabal file is
+          used to discover the only local package.
         
-        haskell-flake currently supports a limited range of syntax for
-        `cabal.project`. Specifically it requires an explicit list of package
-        directories under the "packages" option.
-      '';
-      default =
+          haskell-flake currently supports a limited range of syntax for
+          `cabal.project`. Specifically it requires an explicit list of package
+          directories under the "packages" option.
+        '';
         lib.pipe config.projectRoot [
           haskell-parsers.findPackagesInCabalProject
           (x: config.log.traceDebug "config.haskellProjects.${name}.packages = ${builtins.toJSON x}" x)
@@ -110,6 +39,6 @@ in
           (lib.mapAttrs (_: path: { root = path; }))
         ];
       defaultText = lib.literalMD "autodiscovered by reading `self` files.";
-    };
+      };
   };
 }
