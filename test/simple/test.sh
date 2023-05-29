ed95da66432f85b624d295d8f809204c1da49c71
Sridhar Ratnakumar <srid@srid.ca>
2023-05-29 15:58:22 -0400

    test: test defaults.settings application

--- test/simple/test.sh
+++ test/simple/test.sh
@@ -4,6 +4,10 @@ set -euxo pipefail
 # First, build the flake
 logHeader "Testing nix build"
 nix build ${OVERRIDE_ALL}
+
+# Test defaults.settings module behaviour, viz: separateBinOutput
+test -f result-bin/bin/haskell-flake-test
+
 # Run the devshell test script in a nix develop shell.
 logHeader "Testing nix devshell"
 nix develop ${OVERRIDE_ALL} -c ./test-in-devshell.sh
@@ -13,5 +17,6 @@ nix run ${OVERRIDE_ALL} .#app1
 # Test non-devshell features:
 # Checks
 logHeader "Testing nix flake checks"
-nix --option sandbox false \
-    build ${OVERRIDE_ALL} -L .#check
+nix --option sandbox false build \
+    --no-link --print-out-paths \
+    ${OVERRIDE_ALL} -L .#check
