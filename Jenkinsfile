b0491e20be39f163fbf5f3f8663d21f656bd1a69
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-07-03 16:56:27 -0400

    Add dev flake (#179)
    * Add dev flake
    
    * Build dev flake in Jenkins
    
    * dev: Use treefmt-nix (for CI check)
    
    * treefmt-nix: point to PR

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,13 @@
+// This pipeline uses Groovy functions from https://github.com/juspay/jenkins-nix-ci
 pipeline {
     agent any
     stages {
+        stage ('Dev Flake') {
+            steps {
+                nixBuildAll flakeDir: "./dev", overrideInputs: ["haskell-flake": "."]
+            }
+        }
+        // TODO: Do this as part of dev
         stage ('Tests') {
             steps {
                 sh 'nix run nixpkgs#bash runtest.sh'
---
+++ dev/flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+    mission-control.url = "github:Platonic-Systems/mission-control";
+    # treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.url = "github:srid/treefmt-nix/patch-3"; # https://github.com/numtide/treefmt-nix/pull/89
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = { pkgs, lib, config, ... }: {
+        treefmt.config = {
+          projectRoot = ../.;
+          projectRootFile = "README.md";
+          programs.nixpkgs-fmt.enable = true;
+        };
+        mission-control.scripts = {
+          ex = {
+            description = "Run example";
+            exec = "cd ./example && nix run . --override-input haskell-flake ..";
+          };
+          test = {
+            description = "Run test";
+            exec = "./runtest.sh";
+          };
+          fmt = {
+            description = "Format all Nix files";
+            exec = config.treefmt.build.wrapper;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          # cf. https://haskell.flake.page/devshell#composing-devshells
+          inputsFrom = [ config.mission-control.devShell ];
+        };
+      };
+    };
+}
