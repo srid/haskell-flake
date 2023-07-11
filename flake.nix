815315c0c1ffb3d5c54d0868fb7f7fd6987d8cde
Sridhar Ratnakumar <srid@srid.ca>
2023-07-11 16:31:32 -0400

    Add nixci configuration

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "A `flake-parts` module for Haskell development";
   outputs = { ... }: {
     flakeModule = ./nix/modules;
+
     templates.default = {
       description = "A simple flake.nix using haskell-flake";
       path = builtins.path { path = ./example; filter = path: _: baseNameOf path == "flake.nix"; };
@@ -9,6 +10,12 @@
     templates.example = {
       description = "Example Haskell project using haskell-flake";
       path = builtins.path { path = ./example; filter = path: _: baseNameOf path != "test.sh"; };
+    };
+
+    # https://github.com/srid/nixci
+    nixci = let overrideInputs = { "haskell-flake" = ./.; }; in {
+      dev.dir = "dev";
+      example = { inherit overrideInputs; dir = "example"; };
     };
   };
 }
