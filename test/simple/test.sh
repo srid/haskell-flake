d08dfb50eef96582f69ab0069a1315ca28ca4e5a
Sridhar Ratnakumar <srid@srid.ca>
2024-02-24 08:09:36 -0500

    rm

--- test/simple/test.sh
+++ test/simple/test.sh
@@ -1,11 +1,10 @@
+# TODO: This is being moved to a flake check.
+# Currently doing only what's not being done in the flake check (via nixci).
+
 source ../common.sh
 set -euxo pipefail
 
 rm -f result result-bin
-
-# First, build the flake
-logHeader "Testing nix build"
-nix build ${OVERRIDE_ALL}
 
 # Test defaults.settings module behaviour, viz: haddock
 nix build ${OVERRIDE_ALL} .#default^doc && {
@@ -16,10 +15,3 @@ nix build ${OVERRIDE_ALL} .#default^doc && {
 # Run the devshell test script in a nix develop shell.
 logHeader "Testing nix devshell"
 nix develop ${OVERRIDE_ALL} -c ./test-in-devshell.sh
-# Run the cabal executable as flake app
-logHeader "Testing nix flake app (cabal exe)"
-nix run ${OVERRIDE_ALL} .#app1
-# Test non-devshell features:
-# Checks
-logHeader "Testing nix flake checks"
-nix flake check ${OVERRIDE_ALL}
