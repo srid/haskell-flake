9817cb6cf35abfbe4ec86f01ddfe586fb06b0e2d
Sridhar Ratnakumar <srid@srid.ca>
2024-02-24 10:37:04 -0500

    remove legacy stuff

--- doc/test.sh
+++
@@ -1,6 +0,0 @@
-SYSTEM=$(nix eval --impure --expr builtins.currentSystem)
-HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-nix build --override-input haskell-flake ${HERE}/.. \
-    --option log-lines 1000 --show-trace \
-    "github:hercules-ci/flake.parts-website#checks.${SYSTEM}.linkcheck"
