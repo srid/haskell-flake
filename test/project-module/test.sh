35995aabec6a2054252b4bf5a5fdaff04ac225f1
Sridhar Ratnakumar <srid@srid.ca>
2024-02-24 08:39:02 -0500

    port test/project-module

--- test/project-module/test.sh
+++
@@ -1,9 +0,0 @@
-source ../common.sh
-set -euxo pipefail
-
-# First, build the flake
-logHeader "Testing nix build"
-nix build ${OVERRIDE_ALL}
-
-logHeader "Testing nix devshell"
-nix develop ${OVERRIDE_ALL} -c echo
