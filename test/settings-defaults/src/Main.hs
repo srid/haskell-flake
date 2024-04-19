cca3386dd464adeeb74fed93d7157eb6d4acf4b6
shivaraj-bh <sbh69840@gmail.com>
2024-04-19 07:19:03 +0530

    use the function from random for completeness

--- test/settings-defaults/src/Main.hs
+++ test/settings-defaults/src/Main.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import System.Random
+
 main :: IO ()
 main = do
-  putStrLn "Hello"
+  -- Generate a random number between 1 and 100 (inclusive)
+  randomNumber <- randomRIO (1, 100) :: IO Int
+  putStrLn $ "Hello " ++ show randomNumber
