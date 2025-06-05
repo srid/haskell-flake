15a55873c46755c5b8b4082424eb367cff34b7a0
bglgwyng <bglgwyng@gmail.com>
2025-06-05 11:38:28 +0900

    Fix typo

--- nix/modules/project/settings/all.nix
+++ nix/modules/project/settings/all.nix
@@ -420,7 +420,7 @@ in
     custom = {
       type = types.functionTo types.package;
       description = ''
-        A custom funtion to apply on the Haskell package.
+        A custom function to apply on the Haskell package.
 
         Use this only if none of the existing settings are suitable.
 
