ad0b25bfa9a8c072d2a1725d0cf60c058a715f37
Lin Jian <me@linj.tech>
2025-09-15 08:23:55 +0800

    Apply the custom overlay first
    Close #438

--- nix/modules/project/settings/default.nix
+++ nix/modules/project/settings/default.nix
@@ -75,6 +75,7 @@ in
               });
             }).config;
             cfg = traceSettings name cfg';
+            # NOTE: apply custom first for better UX
             # HACK: buildFromSdist must apply *last*
             # cf. https://github.com/srid/haskell-flake/pull/252
             # In future, we can refactor this as part of https://github.com/srid/haskell-flake/issues/285
@@ -82,12 +83,11 @@ in
             # later appears it fuck up the former otherwise.
             # NOTE: separateIntermediatesOutput must apply after buildFromSdist, otherwise
             # the `installIntermediatesPhase` is set on the `sdist`'s drv instead of the package's.
-            impl = builtins.removeAttrs cfg.impl [ "buildFromSdist" "removeReferencesTo" "separateIntermediatesOutput" ];
-            fns = lib.attrValues impl ++ [ cfg.impl.buildFromSdist cfg.impl.removeReferencesTo cfg.impl.separateIntermediatesOutput ];
+            impl = builtins.removeAttrs cfg.impl [ "custom" "buildFromSdist" "removeReferencesTo" "separateIntermediatesOutput" ];
+            fns = [ cfg.impl.custom ] ++ lib.attrValues impl ++ [ cfg.impl.buildFromSdist cfg.impl.removeReferencesTo cfg.impl.separateIntermediatesOutput ];
           in
           lib.pipe super.${name} (
             # TODO: Do we care about the *order* of overrides?
-            # Might be relevant for the 'custom' option.
             lib.concatMap
               (impl: impl)
               fns
