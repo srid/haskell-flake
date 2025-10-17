7f0bb2a7445c86223694ffb974946e63bdf9fac7
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2025-10-17 16:36:37 -0400

    WIP

--- vira.hs
+++ vira.hs
@@ -1,8 +1,20 @@
 -- CI configuration <https://vira.nixos.asia/>
 \ctx pipeline ->
-  let 
+  let
     isMaster = ctx.branch == "master"
   in pipeline
-     { signoff.enable = True
+     { build.flakes =
+         [ "./dev" { overrideInputs = [("haskell-flake", ".")] }
+         , "./doc" { overrideInputs = [("haskell-flake", ".")] }
+         , "./example" { overrideInputs = [("haskell-flake", ".")] }
+         , "./nix/haskell-parsers/test" { overrideInputs = [("haskell-parsers", "./nix/haskell-parsers")] }
+         , "./test/simple" { overrideInputs = [("haskell-flake", ".")] }
+         , "./test/cabal2nix" { overrideInputs = [("haskell-flake", ".")] }
+         , "./test/with-subdir" { overrideInputs = [("haskell-flake", ".")] }
+         , "./test/project-module" { overrideInputs = [("haskell-flake", ".")] }
+         , "./test/settings-defaults" { overrideInputs = [("haskell-flake", ".")] }
+         , "./test/otherOverlays" { overrideInputs = [("haskell-flake", ".")] }
+         ]
+     , signoff.enable = True
      , cache.url = if isMaster then Just "https://cache.nixos.asia/oss" else Nothing
      }
