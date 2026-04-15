d8a51992c4645eee9ea0e1153afefba0d8b8599f
Sridhar Ratnakumar <srid@srid.ca>
2026-04-15 14:53:41 -0400

    Replace community.flake.parts with direct emanote for docs
    Switch doc site from cfp (community.flake.parts) to plain emanote,
    simplifying the dependency tree significantly.

--- doc/flake.nix
+++ doc/flake.nix
@@ -1,58 +1,29 @@
 {
   nixConfig = {
-    extra-substituters = "https://srid.cachix.org";
-    extra-trusted-public-keys = "srid.cachix.org-1:3clnql5gjbJNEvhA/WQp7nrZlBptwpXnUk6JAv8aB2M=";
+    extra-substituters = "https://cache.nixos.asia/oss";
+    extra-trusted-public-keys = "oss:KO872wNJkCDgmGN3xy9dT89WAhvv13EiKncTtHDItVU=";
   };
 
   inputs = {
-    cfp.url = "github:flake-parts/community.flake.parts";
-    nixpkgs.follows = "cfp/nixpkgs";
-    flake-parts.follows = "cfp/flake-parts";
-
-    # flake-parts-website.url = "github:hercules-ci/flake.parts-website";
-    # flake-parts-website.inputs.haskell-flake.follows = "haskell-flake";
-    # flake-parts-website.inputs.flake-parts.follows = "flake-parts";
-
-    haskell-flake.url = "github:srid/haskell-flake";
+    emanote.url = "github:srid/emanote";
+    emanote.inputs.emanote-template.follows = "";
+    nixpkgs.follows = "emanote/nixpkgs";
+    flake-parts.follows = "emanote/flake-parts";
   };
 
   outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [
-        inputs.cfp.flakeModules.default
-      ];
-      perSystem = { self', inputs', pkgs, system, ... }: {
-        flake-parts-docs = {
-          enable = true;
-          modules."haskell-flake" = {
-            path = self;
-            pathString = ".";
+      imports = [ inputs.emanote.flakeModule ];
+      perSystem = { self', pkgs, system, ... }: {
+        emanote = {
+          sites = {
+            default = {
+              layers = [{ path = ./.; pathString = "."; }];
+            };
           };
         };
         formatter = pkgs.nixpkgs-fmt;
-
-        # Disable linkcheck due to upstream issue
-        # https://github.com/hercules-ci/flake.parts-website/issues/1367
-        /*
-        checks.linkcheck = inputs'.flake-parts-website.checks.linkcheck;
-        packages.flake-parts = inputs'.flake-parts-website.packages.default;
-        apps.flake-parts.program = pkgs.writeShellApplication {
-          name = "docs-flake-parts";
-          meta.description = "Open flake.parts docs, previewing local haskell-flake version";
-          text = ''
-            DOCSHTML="${self'.packages.flake-parts}/options/haskell-flake.html"
-
-            if [ "$(uname)" == "Darwin" ]; then
-              open $DOCSHTML
-            else 
-              if type xdg-open &>/dev/null; then
-                xdg-open $DOCSHTML
-              fi
-            fi
-          '';
-        };
-        */
       };
     };
 }
